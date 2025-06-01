38a0f7993da115336703d0cea082597dc4db55cc
Selamawit Dereje <selamawitd869@gmail.com>
2025-06-01 14:08:46 +0300

    Create 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,9 @@
+const count = parseInt(process.argv[2]);
+
+if (isNaN(count)) {
+  console.log('Missing number of occurrences');
+} else if (count > 0) {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+}
